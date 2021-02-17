02553b37463115215eab6f498b4fca44848f8a78
AdeenaLathiya <adeenalathiya@gmail.com>
2021-02-17 16:22:00 +0500

    SideBar

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -4,17 +4,22 @@ import SidebarOption from "./SidebarOption";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import HomeIcon from "@material-ui/icons/Home";
 import PersonIcon from "@material-ui/icons/Person";
+import { Button } from "@material-ui/core";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       {/* Twitter Icon */}
       <TwitterIcon />
-      {/* Sidebar Option */}
 
-      <SidebarOption Icon={HomeIcon} text="Home" />
+      {/* Sidebar Option */}
+      <SidebarOption active Icon={HomeIcon} text="Home" />
       <SidebarOption Icon={PersonIcon} text="Profile" />
-      {/* <SidebarOption text="Tweet" /> */}
+
+      {/* Tweet Button */}
+      <Button variant="outlined" className="sidebar-tweet" fullWidth>
+        Tweet
+      </Button>
     </div>
   );
 }
