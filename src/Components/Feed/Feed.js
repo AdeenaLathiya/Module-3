38e0a9ad8390422601a3ac1703e67b9def4bda2b
AdeenaLathiya <adeenalathiya@gmail.com>
2021-02-18 17:24:05 +0500

    Tweet

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -3,6 +3,7 @@ import "./Feed.css";
 import TweetBox from "./TweetBox";
 import Post from "./Post";
 import db from "../../firebase";
+import FlipMove from "react-flip-move";
 
 function Feed() {
   const [posts, setPosts] = useState([]);
@@ -19,21 +20,24 @@ function Feed() {
       <div className="feed-header">
         <h2>Home</h2>
       </div>
-      
+
       {/* TweetBox */}
       <TweetBox />
 
       {/* Posts */}
-      {posts.map((post) => (
-        <Post
-          displayName={post.displayName}
-          userName={post.userName}
-          verified={post.verified}
-          text={post.text}
-          avatar={post.avatar}
-          image={post.image}
-        />
-      ))}
+      <FlipMove>
+        {posts.map((post) => (
+          <Post
+            key={post.text} //documentid aaegi yahan pr doc.id
+            displayName={post.displayName}
+            userName={post.userName}
+            verified={post.verified}
+            text={post.text}
+            avatar={post.avatar}
+            image={post.image}
+          />
+        ))}
+      </FlipMove>
     </div>
   );
 }
--- src/Components/Feed/Post.js
+++ src/Components/Feed/Post.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import "./Post.css";
 import { Avatar } from "@material-ui/core";
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
@@ -7,37 +7,39 @@ import RepeatIcon from "@material-ui/icons/Repeat";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import PublishIcon from "@material-ui/icons/Publish";
 
-function Post({ displayName, userName, verified, text, image, avatar }) {
-  return (
-    <div className="post">
-      <div className="post-avatar">
-        <Avatar src={avatar} />
-      </div>
-      <div className="post-body">
-        <div className="post-header">
-          <div className="post-headerText">
-            <h3>
-              {displayName}{" "}
-              <span className="post-headerSpecial">
-                {verified && <VerifiedUserIcon className="post-badge" />} @
-                {userName}
-              </span>
-            </h3>
+const Post = forwardRef(
+  ({ displayName, userName, verified, text, image, avatar }, ref) => {
+    return (
+      <div className="post" ref={ref}>
+        <div className="post-avatar">
+          <Avatar src={avatar} />
+        </div>
+        <div className="post-body">
+          <div className="post-header">
+            <div className="post-headerText">
+              <h3>
+                {displayName}{" "}
+                <span className="post-headerSpecial">
+                  {verified && <VerifiedUserIcon className="post-badge" />} @
+                  {userName}
+                </span>
+              </h3>
+            </div>
+            <div className="post-headerDescription">
+              <p>{text}</p>
+            </div>
           </div>
-          <div className="post-headerDescription">
-            <p>{text}</p>
+          <img src={image} alt="" />
+          <div className="post-footer">
+            <ChatBubbleOutlineIcon fontSize="small" />
+            <RepeatIcon fontSize="small" />
+            <FavoriteBorderIcon fontSize="small" />
+            <PublishIcon fontSize="small" />
           </div>
         </div>
-        <img src={image} alt="" />
-        <div className="post-footer">
-          <ChatBubbleOutlineIcon fontSize="small" />
-          <RepeatIcon fontSize="small" />
-          <FavoriteBorderIcon fontSize="small" />
-          <PublishIcon fontSize="small" />
-        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
 
 export default Post;
