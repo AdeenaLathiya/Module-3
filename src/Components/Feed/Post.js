8af47b1796b4730387397646726a7cf4626e8f61
AdeenaLathiya <adeenalathiya@gmail.com>
2021-02-18 16:36:34 +0500

    Firebase

--- src/Components/Feed/Post.js
+++ src/Components/Feed/Post.js
@@ -2,40 +2,38 @@ import React from "react";
 import "./Post.css";
 import { Avatar } from "@material-ui/core";
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
-import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
-import RepeatIcon from '@material-ui/icons/Repeat';
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-import PublishIcon from '@material-ui/icons/Publish';
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import PublishIcon from "@material-ui/icons/Publish";
 
 function Post({ displayName, userName, verified, text, image, avatar }) {
   return (
     <div className="post">
       <div className="post-avatar">
-        <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
+        <Avatar src={avatar} />
       </div>
       <div className="post-body">
         <div className="post-header">
           <div className="post-headerText">
             <h3>
-              Adeena{" "}
+              {displayName}{" "}
               <span className="post-headerSpecial">
-                <VerifiedUserIcon className="post-badge" /> @adeena
+                {verified && <VerifiedUserIcon className="post-badge" />} @
+                {userName}
               </span>
             </h3>
           </div>
           <div className="post-headerDescription">
-            <p>This is a challenge</p>
+            <p>{text}</p>
           </div>
         </div>
-        <img
-          src="https://media.giphy.com/media/XybYbEoykTGq5g5kco/giphy.gif"
-          alt=""
-        />
+        <img src={image} alt="" />
         <div className="post-footer">
-          <ChatBubbleOutlineIcon fontSize="small"/>
+          <ChatBubbleOutlineIcon fontSize="small" />
           <RepeatIcon fontSize="small" />
-          <FavoriteBorderIcon fontSize="small"/>
-          <PublishIcon fontSize="small"/>
+          <FavoriteBorderIcon fontSize="small" />
+          <PublishIcon fontSize="small" />
         </div>
       </div>
     </div>
